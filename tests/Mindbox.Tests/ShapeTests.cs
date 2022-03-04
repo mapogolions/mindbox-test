9f6b7e0d69867254f328bc0b497f8c9271c2e4a8
mapogolions <ivanovpavelalex45@gmail.com>
2022-03-04 16:26:47 +0500

    basic tests

---
+++ tests/Mindbox.Tests/ShapeTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace Mindbox.Tests;
+
+public class ShapeTest
+{
+    [Fact]
+    public void ShuldReturnZeroIfSidesOfTriangleAreZero()
+    {
+        var triangle = new Triangle((a: 0, b: 0, c: 0));
+        Assert.Equal(0, triangle.Area());
+    }
+
+    [Fact]
+    public void ShouldReturnZeroIfRadiusOfCircleIsZero()
+    {
+        var circle = new Circle(0);
+        Assert.Equal(0, circle.Area());
+    }
+}
