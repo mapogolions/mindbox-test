4589d57583765f16f61470cd7f2ea3253d7d1bb8
mapogolions <ivanovpavelalex45@gmail.com>
2022-03-04 16:07:06 +0500

    initial commit

---
+++ src/Mindbox/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Mindbox;
+public class Class1
+{
+
+}
---
+++ tests/Mindbox.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Mindbox.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
