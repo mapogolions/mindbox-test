fc06afc9e095ab94bc5cf98a828cdceedf57e885
mapogolions <ivanovpavelalex45@gmail.com>
2022-03-09 19:37:15 +0500

    update sql script

--- script.sql
+++ script.sql
@@ -1,6 +1,18 @@
+USE master
+GO
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'MindboxTest'
+)
+CREATE DATABASE MindBoxTest
+GO
+
+USE MindboxTest
+
+DROP TABLE IF EXISTS dbo.ProductCategories
 DROP TABLE IF EXISTS dbo.Products
 DROP TABLE IF EXISTS dbo.Categories
-DROP TABLE IF EXISTS dbo.ProductsCategories
 
 CREATE TABLE dbo.Products
 (
@@ -10,15 +22,17 @@ CREATE TABLE dbo.Products
 
 CREATE TABLE dbo.Categories
 (
-    CategoriesId int IDENTITY PRIMARY KEY,
+    CategoryId int IDENTITY PRIMARY KEY,
     CategoryName NVARCHAR(100) NOT NULL,
 )
 
-CREATE TABLE dbo.ProductsCategories
+CREATE TABLE dbo.ProductCategories
 (
     ProductId INT NOT NULL,
-    CategoryId INT NOT NULL
-    CONSTRAINT Pk_ProductsCategories_ProductIdCategoryId PRIMARY KEY (ProductId, CategoryId)
+    CategoryId INT NOT NULL,
+    CONSTRAINT Pk_ProductCategories_ProductIdCategoryId PRIMARY KEY (ProductId, CategoryId),
+    CONSTRAINT Fk_ProductCategories_Products_ProductId FOREIGN KEY (ProductId) REFERENCES dbo.Products (ProductId),
+    CONSTRAINT FK_ProductCategories_Categories_CategoryId FOREIGN KEY (CategoryId) REFERENCES dbo.Categories (CategoryId)
 )
 
 INSERT INTO dbo.Products (ProductName)
@@ -27,19 +41,19 @@ VALUES ('foo'), ('bar'), ('baz')
 INSERT INTO dbo.Categories (CategoryName)
 VALUES ('cat1'), ('cat2'), ('cat3')
 
-INSERT INTO dbo.ProductsCategories (ProductId, CategoryId)
+INSERT INTO dbo.ProductCategories (ProductId, CategoryId)
 VALUES (1, 1), (1, 2), (2, 1)
 
 
 SELECT prods.ProductName as [Product Name], tmp.CategoryName as [Category Name]
 FROM dbo.Products as prods
 LEFT JOIN
-    (SELECT * FROM dbo.Categories as cats
-    INNER JOIN dbo.ProductsCategories as prodcats
-    ON cats.CategoriesId = prodcats.CategoryId) as tmp
+    (SELECT cats.CategoryId, cats.CategoryName, prodcats.ProductId FROM dbo.Categories as cats
+    INNER JOIN dbo.ProductCategories as prodcats
+    ON cats.CategoryId = prodcats.CategoryId) as tmp
 ON prods.ProductId = tmp.ProductId
 
 
+DROP TABLE IF EXISTS dbo.ProductCategories
 DROP TABLE IF EXISTS dbo.Products
 DROP TABLE IF EXISTS dbo.Categories
-DROP TABLE IF EXISTS dbo.ProductsCategories
